4c54bde6a1cc7a72fcabf54741b7d349bfc0d2d1
Guido Dipietro <dipietroguido@gmail.com>
2021-05-20 13:23:30 -0300

    taoyu

---
+++ GuidoDipietro/tao_birthday/README.md
@@ -0,0 +1,10 @@
+# A birthday party unlike any other
+
+Tao Yu, as well as everybody else, has a birthday.  
+
+The day it comes, you better be well prepared.  
+
+Tao Yu himself once stated: "I dislike birthdays!" Therefore, we shouldn't wish him a happy birthday.  
+
+
+If you're invited, come and join us on this birthday celebration with cake and plants.
---
+++ GuidoDipietro/tao_birthday/birth.c
@@ -0,0 +1,49 @@
+// 20-May-2021
+
+//         ^      ^
+//        | |    | |     believe or not, that is a cake
+//        | |    | |
+#define ____(_,__,___)              ___##__##_
+#define ______(_____)                  _____
+#define ___(_,_____)                 _____##_       // waffles
+#define _____(____)                   (____*)
+#define _(_______)                  put##_______
+
+#define iseach {8750617639641968494,7017848968695079456,8462115405068247161,93842070131744,}
+#define int_ x;wow[]={4*4,7-10,1+'\n',7*3,'/'/'/'+(1<<4)};
+
+                // oops i just dropped some
+
+                                       ___  (
+                                          _
+                                       ,              // piranha plant food
+
+int) main() {
+           ___                      (ng  ,    lo) //nom nom
+           ___                       (ng  ,  lo) 
+           ____                        (o,a,t)
+                                         [ ]
+           ______                        (=)
+           ____                      (each, s,i)
+           ____                      (   ,;,   )
+           ___                       (tf  ,prin)
+("%s\n",   _____                     (  char   )
+
+
+            tao /* not my birthday! */ ); /*sad emote made of several*/char
+
+
+                                     y
+                                  [5]={[0
+                                    ...
+                                  4]=100};   "so I baked another cake, looks ugly but tastes delicious"
+                                ;for(;x[y];              // -Tua O, University of Wakanda, 1427
+                                _(char)(*(
+                                y+x)+*(wow
+                                +x)),x=-~x)
+
+/*now tao is happy yayz look:*/
+
+          ;}
+
+// FACE REVEAL OMG!
