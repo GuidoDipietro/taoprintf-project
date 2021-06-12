126b0e8a3ece47041cd13b1cb231b440a68ece02
Guido Dipietro <dipietroguido@gmail.com>
2021-06-12 18:30:10 -0300

    taoyu

---
+++ GuidoDipietro/tao_ngraphs/README.md
@@ -0,0 +1,6 @@
+# Ok
+
+Yeah.  
+I don't know why GuyClicking had never told me about digraphs and trigraphs.  
+
+The rest is just basic C knowledge.
---
+++ GuidoDipietro/tao_ngraphs/tao.c
@@ -0,0 +1,21 @@
+#define A ??<static
+#define O O;;;;;;;;
+
+%:define _ p??=??=u%:%:t??=#s(\
+                                     "taoyu"??/ 
+    )
+main(
+    )
+A
+O
+    
+return!
+
+_
+
+??!??!??-'??-'??' '\' ';%> // what even is this?????/
+int main(void) {\
+    printf("Please report any bugs.");\
+    return 0;\
+}
+
